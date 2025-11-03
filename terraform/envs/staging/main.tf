4731b0709c45825d9fb0625c317b0464436e0e7f
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-03 22:36:46 +0530

    Update main.tf

--- terraform/envs/staging/main.tf
+++ terraform/envs/staging/main.tf
@@ -1,7 +1,7 @@
 module "route53" {
   source        = "../../modules/route53"
   env           = "staging"
-  domain_name   = "myapp.example.com"
+  domain_name   = "https://vijaychandra.site"
   alb_dns_name  = module.ecs.alb_dns_name
   alb_zone_id   = data.aws_lb.main.zone_id
 }
