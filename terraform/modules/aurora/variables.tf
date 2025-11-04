10262cda5f73d5f61e98984fe9adf72790ba1c2c
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-04 13:28:44 +0530

    Update variables.tf

--- terraform/modules/aurora/variables.tf
+++ terraform/modules/aurora/variables.tf
@@ -28,6 +28,10 @@ variable "db_username" {
 variable "db_name" {
   default = "appdb"
 }
+variable "kms_key_arn" {
+  description = "KMS key ARN for Aurora encryption"
+  type        = string
+}
 
 
 
