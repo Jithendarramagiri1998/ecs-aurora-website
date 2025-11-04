3a580632ce279abee6e5afb6aaebfc202b56242d
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-04 12:25:15 +0530

    Update variables.tf

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -2,3 +2,7 @@ variable "env" {
   description = "Environment name (dev, staging, prod)"
   type        = string
 }
+variable "project_name" {
+  type        = string
+  description = "Name of the project"
+}
