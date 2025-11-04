dbed896c6c9800c2affa5d0766809d76affaa9ef
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-04 13:36:01 +0530

    Update variables.tf

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -1,5 +1,5 @@
 variable "env" {
-  description = "Environment name (dev, staging, prod)"
+  description = "Environment name (dev/staging/prod)"
   type        = string
   default     = "dev"
 }
@@ -8,4 +8,10 @@ variable "project_name" {
   description = "Project name"
   type        = string
   default     = "ecs-aurora"
+}
+
+variable "db_password" {
+  description = "Aurora DB master password"
+  type        = string
+  default     = "MySecurePassword123!"
 }
