ea00d55236f6e6b70498267c84ba825b620234b6
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-07 17:33:55 +0530

    Update main.tf

--- terraform/envs/staging/main.tf
+++ terraform/envs/staging/main.tf
@@ -1,7 +1,15 @@
+module "ecs" {
+  source             = "../../modules/ecs"
+  env                = var.env
+  public_subnet_ids  = var.public_subnet_ids
+  vpc_id             = var.vpc_id
+  ...
+}
+
 module "route53" {
   source       = "../../modules/route53"
   env          = var.env
   domain_name  = "vijaychandra.site"
-  alb_dns_name = aws_lb.ecs_alb.dns_name
-  alb_zone_id  = aws_lb.ecs_alb.zone_id
+  alb_dns_name = module.ecs.alb_dns_name
+  alb_zone_id  = module.ecs.alb_zone_id
 }
