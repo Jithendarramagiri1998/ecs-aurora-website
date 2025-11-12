3f0f37598ad2d823ef5a9d4c9e814e770367e74c
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-11-12 15:16:11 +0530

    Update backend.tf

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-states-1234"
-    key            = "dev/terraform.tfstate"
+    key            = "staging/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
