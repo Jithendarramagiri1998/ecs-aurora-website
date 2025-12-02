166e3221e2e9ad1884266e5edc9fbcd33cfe3722
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-12-02 23:34:44 +0530

    Update variables.tf

--- terraform/envs/staging/variables.tf
+++ terraform/envs/staging/variables.tf
@@ -1,17 +1,50 @@
+########################################################
+# Environment Name
+########################################################
 variable "env" {
   description = "Environment name (dev, staging, prod)"
   type        = string
-  default     = "staging"
 }
-variable "container_image" {
-  description = "ECR image URI"
+
+########################################################
+# Project Name
+########################################################
+variable "project_name" {
+  description = "Project/application name"
   type        = string
 }
 
-variable "vpc_id" {}
-variable "public_subnet_ids" {}
-variable "private_subnet_ids" {}
-variable "db_host" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
+########################################################
+# Container Image (ECR URI)
+########################################################
+variable "container_image" {
+  description = "ECR image URI for ECS deployment"
+  type        = string
+}
+
+########################################################
+# Database Username
+########################################################
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "admin"
+}
+
+########################################################
+# Database Name
+########################################################
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "appdb"
+}
+
+########################################################
+# Database Password
+########################################################
+variable "db_password" {
+  description = "Database password for Aurora cluster"
+  type        = string
+  sensitive   = true
+}
