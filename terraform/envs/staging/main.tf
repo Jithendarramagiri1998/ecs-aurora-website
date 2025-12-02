d1f2213a06e33560b36d6de395a027577a0dfcdd
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-12-02 23:34:07 +0530

    Update main.tf

--- terraform/envs/staging/main.tf
+++ terraform/envs/staging/main.tf
@@ -1,24 +1,96 @@
+#############################################
+# AWS Provider (Staging)
+#############################################
+provider "aws" {
+  region = "us-east-1"
+}
+
+#############################################
+# VPC for STAGING (Independent)
+#############################################
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name        = var.project_name
+  env                 = var.env
+
+  vpc_cidr            = "10.20.0.0/16"
+  azs                 = ["us-east-1a", "us-east-1b"]
+
+  public_subnets      = ["10.20.1.0/24", "10.20.2.0/24"]
+  private_app_subnets = ["10.20.3.0/24", "10.20.4.0/24"]
+  private_db_subnets  = ["10.20.5.0/24", "10.20.6.0/24"]
+}
+
+#############################################
+# ECS Cluster + Application (STAGING)
+#############################################
 module "ecs" {
-  source             = "../../modules/ecs"
+  source = "../../modules/ecs"
+
   env                = var.env
-  public_subnet_ids  = var.public_subnet_ids
-  private_subnet_ids = var.private_subnet_ids
-  vpc_id             = var.vpc_id
-# DB details
-  db_host     = var.db_host
+  container_image    = var.container_image
+
+  # Network (coming from staging VPC)
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_app_subnet_ids
+
+  # DB details (Aurora will create DB endpoint)
+  db_host     = module.aurora.db_cluster_endpoint
   db_name     = var.db_name
   db_username = var.db_username
   db_password = var.db_password
-
-  # Container image to deploy
-  container_image = var.container_image
-
 }
 
+#############################################
+# Aurora MySQL Cluster (STAGING)
+#############################################
+module "aurora" {
+  source = "../../modules/aurora"
+
+  project_name       = var.project_name
+  env                = var.env
+
+  vpc_id             = module.vpc.vpc_id
+  private_db_subnets = module.vpc.private_db_subnet_ids
+
+  ecs_sg_id          = module.ecs.ecs_sg_id
+  db_password        = var.db_password
+}
+
+#############################################
+# Route53 - Create record for STAGING ALB
+#############################################
 module "route53" {
-  source       = "../../modules/route53"
-  env          = var.env
-  domain_name  = "vijaychandra.site"
+  source      = "../../modules/route53"
+
+  env         = var.env
+  domain_name = "vijaychandra.site"
+
+  # ALB data from ECS module
   alb_dns_name = module.ecs.alb_dns_name
   alb_zone_id  = module.ecs.alb_zone_id
+}
+
+#############################################
+# SNS Alerts (STAGING)
+#############################################
+module "sns" {
+  source      = "../../modules/sns"
+  env         = var.env
+  alert_email = "alerts@mydomain.com"
+}
+
+#############################################
+# CloudWatch Alarms (STAGING)
+#############################################
+module "cloudwatch" {
+  source           = "../../modules/cloudwatch"
+
+  env              = var.env
+  ecs_cluster_name = module.ecs.cluster_name
+  db_cluster_id    = module.aurora.db_cluster_id
+  sns_topic_arn    = module.sns.topic_arn
+  aws_region       = "us-east-1"
 }
